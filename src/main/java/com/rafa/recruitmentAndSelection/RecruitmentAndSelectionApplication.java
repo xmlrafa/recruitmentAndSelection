1b11fe2280d80a3d6e1c1aa1ec9d51ac5703349a
Rafael de Jesus <rafael_rodrigues1993@yahoo.com.br>
2024-06-20 20:23:06 -0300

    Initial commit

---
+++ src/main/java/com/rafa/recruitmentAndSelection/RecruitmentAndSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.rafa.recruitmentAndSelection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAndSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAndSelectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafa/recruitmentAndSelection/RecruitmentAndSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafa.recruitmentAndSelection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAndSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
